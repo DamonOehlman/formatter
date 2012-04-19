d53e40f843dd7bde201cc4b86874335ccd2b4d57
Damon Oehlman <damon.oehlman@sidelab.com>
2012-04-19 15:08:07 +1000

    Added some more tests

--- test/nested.js
+++ test/nested.js
@@ -1,11 +1,37 @@
 var expect = require('expect.js'),
-    formatter = require('../pkg/cjs/formatter');
+    formatter = require('../pkg/cjs/formatter'),
+    testData = {
+        name: 'Bob',
+        
+        address: {
+            street: {
+                number: 15,
+                name: 'West St'
+            },
+            
+            country: 'Australia'
+        }
+    };
 
 describe('nested expression replacement tests', function() {
-    it('should be able to replace simple nested expressions', function() {
-        var test = formatter('Hi there {{ person.name }}!');
+    it('should be able to extract simple values from compound data', function() {
+        var test = formatter('Hi there {{ name }}!');
         
         expect(test).to.be.ok();
-        expect(test({ person: { name: 'Bob' }})).to.equal('Hi there Bob!');
+        expect(test(testData)).to.equal('Hi there Bob!');
+    });
+
+    it('should be able to extract a value 2 levels deep', function() {
+        var test = formatter('Hi there {{ name }} from {{ address.country }}!');
+        
+        expect(test).to.be.ok();
+        expect(test(testData)).to.equal('Hi there Bob from Australia!');
+    });
+    
+    it('should be able to extract a value 3 levels deep', function() {
+        var test = formatter('{{ name }} lives on a street named {{ address.street.name }}');
+        
+        expect(test).to.be.ok();
+        expect(test(testData)).to.equal('Bob lives on a street named West St');
     });
 });
